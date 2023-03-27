612f795b8be0e7b1c75d0debe680fc25fed6f5bd
Georg Kaltenbrunner <69145850+GeorgKaltenbrunner@users.noreply.github.com>
2023-03-27 17:57:31 +0200

    Update validation.py

--- validation.py
+++ validation.py
@@ -47,3 +47,31 @@ def track_validation(order_id):
     increase_finished_after_einschwing()
     count_early_tardy_orders(order_id)
     average_sftt(order_id)
+    
+def final_validation():
+    """
+    This function puts the validation information together and exports it as validation excel.
+    :return: An Excel is created and exported.
+    """
+    early_orders = GlobaleVariables.early_orders
+    tardy_orders = GlobaleVariables.tardy_orders
+    early_ratio = GlobaleVariables.early_orders / GlobaleVariables.finished_after_einschwing
+    tardy_ratio = tardy_orders / GlobaleVariables.finished_after_einschwing
+    avg_sftt = np.array(GlobaleVariables.sftt_track).mean()
+    utilization_station1 = sum(GlobaleVariables.utilization_dict[1]) / GlobaleVariables.SIM_TIME
+    utilization_station2 = sum(GlobaleVariables.utilization_dict[2]) / GlobaleVariables.SIM_TIME
+    utilization_station3 = sum(GlobaleVariables.utilization_dict[3]) / GlobaleVariables.SIM_TIME
+
+    validation_df = pd.DataFrame({'early_orders': early_orders,
+                                  'tardy_orders': tardy_orders,
+                                  'early_ratio': early_ratio,
+                                  'tardy_ratio': tardy_ratio,
+                                  'mean_earliness': np.mean(
+                                      GlobaleVariables.earliness),
+                                  'mean_tardiness': np.mean(
+                                      GlobaleVariables.tardiness),
+                                  'avg_sftt': avg_sftt,
+                                  'util_station_1': utilization_station1,
+                                  'util_station_2': utilization_station2,
+                                  'util_station_3': utilization_station3},
+                                 index=[GlobaleVariables.SIM_ROUND])
